777206de8cfa5650a6d9eca1f3c89c877576eb0f
Johan Sundblad <johan.sundblad@svenskaspel.se>
2014-08-05 18:36:20 +0200

    Initial Commit

---
+++ JS Pong/JSPongAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSPongAppDelegate.h
+//  JS Pong
+//
+//  Created by Johan Sundblad on 2014-08-05.
+//  Copyright (c) 2014 Johan Sundblad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSPongAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JS Pong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JS Pong
+//
+//  Created by Johan Sundblad on 2014-08-05.
+//  Copyright (c) 2014 Johan Sundblad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSPongAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSPongAppDelegate class]));
+    }
+}
